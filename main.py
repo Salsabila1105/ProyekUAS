5ec32849cebf5f713dd3f18466a74d1fbb6aa889
Salsabila1105 <75847941+Salsabila1105@users.noreply.github.com>
2022-02-01 05:41:03 +0000

    temperature

--- main.py
+++ main.py
@@ -53,13 +53,14 @@ class ModernRicecooker(Ricecooker):
 # mebuat object dari clas tv
 Ricecooker_kamar = Ricecooker('YoungMa', '2Liter')
 Ricecooker_tamu = ModernRicecooker('Toshiba', '3Liter')
-
+  
 
 while True:
   print("1) nyalakan Ricecooker")
   print("2) tambakan merk Ricecooker")
   print("3) Hapus merk Ricecooker")
-  print("4) keluar dari program")
+  print("4) Tambah temperature")
+  print("5) keluar dari program")
   pilihan = int(input("silahkan masukan pilihan anda "))
   if(pilihan == 1):
     Ricecooker_kamar.menyalakanRicecooker()
@@ -78,6 +79,14 @@ while True:
     print('merk sekarang:')
     print(Ricecooker_kamar.merk)
   elif(pilihan == 4):
+    try:
+      temperature = int(input('Silahkan masukan temperature : '))
+      settemperature = int(input('Silahkan set temperature : '))
+      newtemperature = temperature + settemperature
+    except:
+      print('temperature error')
+      print('Temperature berhasil diubah, temperature sekarang adalah : ', newtemperature)
+  elif(pilihan == 5):
     print('anda keluar program')
     break
   else:
